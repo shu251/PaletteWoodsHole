db600db115041db92201e1a1010058c47b51764a
shu251 <sarah.hu@whoi.edu>
2021-02-27 12:07:25 -0500

    initial commit

---
+++ R/data.r
@@ -0,0 +1,110 @@
+#' WHOI logo
+#'
+#' A list of HEX colors, n = 4
+#'
+#' @format HEX color elements that match the logo
+#'   ...
+#' @source \url{https://www.whoi.edu}
+"whoi"
+
+
+#' WHOI logo 2
+#'
+#' A list of HEX colors, n = 6
+#'
+#' @format HEX color elements that match the logo
+#'   ...
+#' @source \url{https://www.whoi.edu}
+"whoi2"
+
+
+#' Atlantis
+#'
+#' A list of HEX colors, n = 9
+#'
+#' @format HEX color elements that match the RV Atlantis
+#'   ...
+#' @source \url{https://www.whoi.edu}
+"atlantis"
+
+#' Bikepath
+#'
+#' A list of HEX colors, n = 7, that represent the essence of the Shining Sea Bikeway
+#'
+#' @format HEX color elements that represent the bikepath
+#'   ...
+#' @source \url{https://www.whoi.edu}
+"bikepath"
+
+#' Cranberry bog
+#'
+#' A list of HEX colors, n = 6, that match the majestic cranberry bogs of Massachusetts
+#'
+#' @format HEX color elements that represent a cranberry bog
+#'   ...
+#' @source \url{https://www.cranberries.org/how-cranberries-grow}
+"bog"
+
+
+#' Eel Pond in winter
+#'
+#' A list of HEX colors, n = 6, that convey the beauty of fresh snowfall on Eel Pond in Woods Hole.
+#'
+#' @format HEX color elements that represent the winter in Eel pond
+#'   ...
+#' @source \url{https://woodshole.com/webcams/eel-pond-from-mbl/}
+"eelpond_winter"
+
+#' ROV Jason
+#'
+#' A list of HEX colors, n = 6, that represent ROV Jason
+#'
+#' @format HEX color elements for Jason
+#'   ...
+#' @source \url{https://www.whoi.edu/what-we-do/explore/underwater-vehicles/ndsf-jason/}
+"jason"
+
+#' Rocky beach
+#'
+#' You remember the summertime in Woods Hole. Going to beach may mean a visit to a rocky beach. A list of HEX colors, n = 6, to bring you to the beach.
+#'
+#' @format HEX color elements for rocky beach
+#'   ...
+#' @source \url{}
+"rocky_beach"
+
+#' Sunset winter
+#'
+#' Sunsets on the cape are not to be missed. Even in the winter. 6 HEX color codes to represent that winter sunset vibe.
+#'
+#' @format HEX color elements for wintertime sunset
+#'   ...
+#' @source \url{}
+"sunset_winter"
+
+#' Tulips
+#'
+#' A sign of warmer weather. 9 HEX color code list that will bring you there - there, being the tulips that pop up in late spring.
+#'
+#' @format HEX color elements for tulips
+#'   ...
+#' @source \url{}
+"tulips"
+
+#' West Falmouth Sunset
+#'
+#' You've raced to Chappys to catch the sunset, right? 8 HEX color codes to bring the West Falmouth sunset to you.
+#'
+#' @format HEX color elements for West Falmouth sunset
+#'   ...
+#' @source \url{}
+"wefa_sun"
+
+#' Woods Hole color palette
+#'
+#' Complete list of Woods Hole color palettes available in package.
+#'
+#' @format List of all Woods Hole color palettes available in the package.
+#'   ...
+#' @source \url{}
+"woodshole"
---
+++ R/show_color.R
@@ -0,0 +1,31 @@
+woodshole <- list(
+  whoi <- c("#10B7BD","#12A8E0", "#0F6BB3","#FFFFFF"),
+  whoi2 <- c("#FFFFFF","#0F6BB3","#D9F0F7", "#10B7BD","#B4DFED","#46A7CF"),
+  jason <- c("#044F9A","#fec44f", "#5D86C3", "#fc4e2a", "#B6C8DB", "#2A394F"),
+  atlantis <- c("#9DBABB","#1B334B","#CFBC9F",
+                "#576F70","#091A2A","#301609",
+                "#E0E5E5","#596873","#878A7E"),
+  wefa_sun <- c("#DDC794", "#B5623A", "#F95316","#584438",
+                "#EEBA62","#8C3B25","#9B8E77","#917062"),
+  bikepath <- c("#6C6865","#595E44","#869055","#E1DFEA","#313E1C","#D0D4DC","#BCB277"),
+  bog <- c("#AC7070","#D6D9DA", "#33291B","#C5C7C7", "#937771","#5A4A35"),
+  rocky_beach <- c("#716347","#476AA7","#7299CE","#A2937A","#B2C9E3","#3D3019"),
+  eelpond_winter <- c("#E1E7ED","#C5C9CF","#353638",
+                      "#6A7072","#AAAEB3","#ffffff"),
+  tulips <- c("#D77D36", "#E4BF62", "#79805E",
+              "#E2DBC7","#81A063","#fa9fb5",
+              "#B24236","#fed976","#5A7356"),
+  sunset_winter <- c("#FDEAAB","#E9B27A","#A8B1BD","#B58975","#DBDBD2","#F6E7C1")
+)
+
+#' Prints grid of colors
+#'
+#' @param color input color name
+#'
+#' @return Grid using 'scales' with hex code and color
+#' @export
+#'
+#' @examples show_color(atlantis)
+show_color <- function(color){
+  scales::show_col(color)
+}
